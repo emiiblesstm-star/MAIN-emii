6840590a56c8debb32d711d025895d33977b8abc
Brian <ndanumumo93@gmail.com>
2025-10-20 22:35:54 +0300

    Initial commit for RUGARA-MAIN

---
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '70753', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '70753',
+    'app.deriv.com': '70753',
+    'staging-app.deriv.com': '70753',
+    'goon-puce.vercel.app': '70753',
+});
+
+export default AppIds;
